300d928b7662eec4169a3c1bd138aa55c3db1996
shahrokh <shahrokhelmi@gmail.com>
2022-01-17 14:35:14 +0330

    challenge 1 finished

--- script.js
+++ script.js
@@ -1,38 +1,71 @@
 'use strict';
+//
+// const Person = function(firstName, birthYear){
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+// }
+//
+// const shahrokh = new Person('shahrokh',1993);
+//
+// console.log(shahrokh)
+// console.log(shahrokh instanceof Person)
+// // console.log(shahrokh.calcAge())
+//
+// Person.prototype.calcAge = function(){
+//     console.log(2022-this.birthYear)
+// }
+// console.log(Person.prototype)
+//
+// console.log(shahrokh.__proto__)
+//
+//
+// Person.prototype.species = 'Homo Sapiens';
+//
+// console.log(shahrokh.species) // Homo Sapiens
+//
+// console.log(shahrokh.hasOwnProperty('species')); // false
+// console.log(shahrokh.hasOwnProperty('firstName')) // true
+//
+// console.log(shahrokh.__proto__.__proto__)
+// console.log(shahrokh.__proto__.__proto__.__proto__)
+//
+// console.log(Person.prototype.constructor)
+// console.dir(Person.prototype.constructor)
+//
+//
+// const arr = [1,2,3,4]
+// console.log(arr.__proto__)
+// console.log(arr.__proto__.__proto__)
 
-const Person = function(firstName, birthYear){
-    this.firstName = firstName;
-    this.birthYear = birthYear;
+
+// challenge # 1
+
+const Car = function(make, speed){
+    this.make = make;
+    this.speed = speed;
 }
 
-const shahrokh = new Person('shahrokh',1993);
-
-console.log(shahrokh)
-console.log(shahrokh instanceof Person)
-// console.log(shahrokh.calcAge())
-
-Person.prototype.calcAge = function(){
-    console.log(2022-this.birthYear)
+Car.prototype.acceleration = function(){
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed}Km/h`);
 }
-console.log(Person.prototype)
 
-console.log(shahrokh.__proto__)
+Car.prototype.brake = function(){
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed}Km/h`);
 
+}
 
-Person.prototype.species = 'Homo Sapiens';
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
 
-console.log(shahrokh.species) // Homo Sapiens
+bmw.acceleration()
+bmw.acceleration()
+bmw.brake()
+bmw.brake()
 
-console.log(shahrokh.hasOwnProperty('species')); // false
-console.log(shahrokh.hasOwnProperty('firstName')) // true
+mercedes.acceleration()
+mercedes.acceleration()
+mercedes.brake()
+mercedes.brake()
 
-console.log(shahrokh.__proto__.__proto__)
-console.log(shahrokh.__proto__.__proto__.__proto__)
-
-console.log(Person.prototype.constructor)
-console.dir(Person.prototype.constructor)
-
-
-const arr = [1,2,3,4]
-console.log(arr.__proto__)
-console.log(arr.__proto__.__proto__)
